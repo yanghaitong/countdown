bb39be1a98afcddbd2f2b1b58bb5d502177e15dc
海曈 <99116438+yanghaitong@users.noreply.github.com>
2025-03-13 16:03:18 +0800

    Add files via upload

--- JavaScript/date.js
+++ JavaScript/date.js
@@ -1,3 +1,46 @@
+console.log(
+    '                   _oo0oo_                     \n' +
+    '                  o8888888o										\n' +
+    '                  88" . "88										\n' +
+    '                  (| -_- |)										\n' +
+    '                   O\\ = /O										\n' +
+    "               ____/`---'\\____									\n" +
+    "             .   ' \\\\| |// `.									\n" +
+    '              / \\\\||| : |||// \\								\n' +
+    '           / _||||| -卍- |||||- \\								\n' +
+    '              | | \\\\\\ - /// | |								\n' +
+    "            | \\_| ''\\---/'' | |								\n" +
+    '             \\ .-\\__ `-` ___/-. /							\n' +
+    "          ___`. .' /--.--\\ `. . __							\n" +
+    '       ."" "< `.___\\_<|>_/___. ` >" "".        \n' +
+    '      | | : `- \\`.;`\\ _ /`;.`/ - ` : | |       \n' +
+    '        \\ \\ `-. \\_ __\\ /__ _/ .-` / /          \n' +
+    "======`-.____`-.___\\_____/___.-`____.-'======  \n" +
+    "                   `=---='                     \n" +
+    '.............................................  \n\t\t' +
+    '佛祖镇楼                  BUG辟易                          \n\t' +
+    '佛曰:\n\t\t' +
+    '写字楼里写字间，写字间里程序员；\n\t\t' +
+    '程序人员写程序，又拿程序换酒钱。\n\t\t' +
+    '酒醒只在网上坐，酒醉还来网下眠；\n\t\t' +
+    '酒醉酒醒日复日，网上网下年复年。\n\t\t' +
+    '但愿老死电脑间，不愿鞠躬老板前；\n\t\t' +
+    '奔驰宝马贵者趣，公交自行程序员。\n\t\t' +
+    '别人笑我忒疯癫，我笑自己命太贱；\n\t\t' +
+    '不见满街漂亮妹，哪个归得程序员？'
+);
+
+/**
+ * 警示框
+ *
+ * @author HaiTong
+ */
+const dome = [(
+    "hello，大家好。"
+)];
+
+// alert(dome);
+
 /**
  * new 一个date对象
  * @type {Date}
@@ -30,11 +73,17 @@ let day = date.getDate();
 let next = document.querySelector(".next");
 
 /**
+ * 提示是什么倒计时
+ * @type {Element}
+ */
+let prompts = document.querySelector(".prompt");
+
+
+/**
  * 获取html 今天时间
  * @type {Element}
  */
 let current = document.querySelector(".current");
-
 let days = document.querySelector("#sky");
 let hours = document.querySelector("#hour");
 let minutes = document.querySelector("#divide");
@@ -42,6 +91,9 @@ let seconds = document.querySelector("#second");
 let countdown = document.querySelector(".countdown")
 
 next.innerHTML = `今天是${year}年${month}月${day}日`;
+
+
+prompts.innerHTML = "2026年元旦";
 
 /**
  * 现在时间的方法
@@ -71,7 +123,14 @@ function countTime() {
     let now = +new Date();
     //2、得到指定时间的时间戳
 
-    let last = +new Date("2025-1-1 00:00:00");
+    // 输入倒计时截止的时间
+    let last = +new Date("2026-1-1 00:00:00");
+
+    /**
+     * 2025年2月19日：2025-1-1 00:00:00 2025年元旦、除夕、春节
+     */
+
+
     // 3、计算剩余的毫秒数/1000 == 剩余的秒数
     let count = (last - now) / 1000;
     // 4、转换为天时分秒
@@ -88,6 +147,8 @@ function countTime() {
     s = s < 10 ? '0' + s : s;
     seconds.innerHTML = s;
     if (count <= 0) {
+
+        // 时间截止的提示
         countdown.innerHTML = '除夕到了！！！'
     }
 }
